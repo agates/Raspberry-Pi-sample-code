90315554d4297091c0bb2cb28435b4bacc0f06c0
Alecks Gates <alecks.g@gmail.com>
2018-02-27 23:28:33 -0600

    Move code into a package usable for pip installation.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='atlasrpi',
+    version='1.0.0',
+    description='Atlas Scientific scripts for the Raspberry Pi',
+    license='MIT',
+    packages=['atlasrpi'],
+    author='Alecks Gates',
+    author_email='agates@mail.agates.io',
+    keywords=[],
+    url='https://github.com/agates/atlas-rpi'
+)
